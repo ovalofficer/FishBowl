29b08d47f2b2d8baeaf173581cd6202a5be20bfb
Morgan <mbphillips2001@gmail.com>
2024-05-11 01:02:16 -0400

    Added tank graphics
    Fixed fish behavior
    
    Fixed fish pathing
    
    Added some sound effects
    
    General optimizations and improvements

--- Fish.py
+++ Fish.py
@@ -1,5 +1,30 @@
 import random
+
 import pygame
+
+pygame.mixer.init()
+
+IMAGES = {
+    "food": pygame.image.load('./sprites/Food.png'),
+
+}
+
+FISH_IMAGES: dict = {
+    0: pygame.image.load('./sprites/NeutralFish.png'),
+
+    1: pygame.image.load('./sprites/LonerFish.png'),
+
+    2: pygame.image.load('./sprites/FriendlyFish.png'),
+
+    3: pygame.image.load('./sprites/BubbleFish.png')
+}
+
+SOUNDS: dict = {
+    "bubble_pop": pygame.mixer.Sound("./sounds/bubble-pop.mp3")
+}
+
+for sound in SOUNDS.values():
+    sound.set_volume(0.25)
 
 
 class Bubble:
@@ -18,10 +43,11 @@ class Bubble:
         self.shine_amount = random.randint(1, self.radius // 10 + 1)
 
     def rise(self, bubbles):
-        if self.pos[1] + 8 > 0:
-            self.pos = (self.pos[0] + random.randint(-1, 1), self.pos[1] - 1)
-        else:
+        if self.pos[1] + 8 < 0 or random.randint(0, 1000) == 1:
+            SOUNDS.get("bubble_pop").play()
             bubbles.remove(self)
+        else:
+            self.pos = (self.pos[0] + random.randint(-1, 1), self.pos[1] - 1)
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255, 80), self.pos, self.radius, 1)
@@ -35,31 +61,19 @@ class Food:
     def __init__(self, pos: tuple[int, int] = (0, 0), y_limit: int = 0, nutrition: int = 1000):
         self.pos = pos
         self.floor = y_limit
-        self.nutrition = nutrition
+        # self.nutrition = nutrition #-- NOT YET IMPLEMENTED
 
-    def fall(self):
+    def fall(self, foods):
         if self.pos[1] + 8 < self.floor:
             self.pos = (self.pos[0] + random.randint(-1, 1), self.pos[1] + 1)
+        else:
+            foods.remove(self)
 
-    def draw(self, screen, color):
-        pygame.draw.rect(screen, color, (*self.pos, 8, 8))
+    def draw(self, screen):
+        screen.blit(pygame.transform.scale(IMAGES.get("food"), (10, 10)), self.pos)
 
 
 class Fish:
-    personalities = {
-        0: {"name": "Neutral",
-            "description": "This fish has no preference for where to be. They will wander the tank aimlessly "
-                           "and chase food when they are hungry."},
-        1: {"name": "Timid",
-            "description": "This fish prefers to stay away from the others. They will hang around the corners of the "
-                           "tank and chase food only if another fish isn't going for it already."},
-        2: {"name": "Friendly",
-            "description": "This fish prefers to stay near the others. They will hang around the middle of the tank, "
-                           "and race other fish to get food."},
-        3: {"name": "Bubble Chaser",
-            "description": "This fish does not wander like the rest. They only want to chase bubbles. They will eat "
-                           "only when necessary."}
-    }
 
     def __init__(self, name, pos: tuple[int, int] = (0, 0), bounds: tuple[int, int] = (100, 100), hunger: int = 1000,
                  personality: int = 0):
@@ -68,13 +82,18 @@ class Fish:
         self.hunger = hunger
         self.bounds = bounds
         self.closest_food = None
-        self.closest_fish = None
+        self.closest_fish = self
+        self.facing_left = False
         self.wander_goal: tuple = (0, 0)
         self.personality = personality
         self.assign_new_wander_goal()
 
+    def move(self, amount_x: int = 0, amount_y: int = 0):
+        self.facing_left = amount_x <= 0
+        self.pos = (self.pos[0] + amount_x, self.pos[1] + amount_y)
+
     def eat(self):
-        self.hunger += 1000
+        self.hunger += 1200
 
     def increase_hunger(self):
         self.hunger = max(self.hunger - 1, 0)
@@ -84,14 +103,17 @@ class Fish:
         return len(foods) > 0
 
     def is_hungry(self):
-        return self.hunger <= 700
+        if self.personality == 3:
+            return self.hunger <= 300
+        else:
+            return self.hunger <= 700
 
-    def is_within(self) -> bool:
-        return not (self.pos[0] <= 0 or self.pos[0] >= self.bounds[0] or self.bounds[1] <= 0 or self.bounds[1] >=
+    def is_within(self, pos) -> bool:
+        return not (pos[0] <= 0 or pos[0] >= self.bounds[0] or pos[1] <= 0 or pos[1] >=
                     self.bounds[1])
 
     def get_random_map_point(self):
-        return random.randrange(0, self.bounds[0]), random.randrange(0, self.bounds[1])
+        return random.randint(0, self.bounds[0]), random.randint(0, self.bounds[1])
 
     def assign_new_wander_goal(self) -> None:
         self.wander_goal = self.get_random_map_point()
@@ -111,7 +133,7 @@ class Fish:
     def get_distance_from_self(self, xy) -> float:
         return self.get_distance_to_point(self.pos, xy)
 
-    def get_distance_to_target(self, target: Food) -> float:
+    def get_distance_to_target(self, target) -> float:
         return self.get_distance_from_self(target.pos)
 
     def get_closest_ent(self, ents):
@@ -131,7 +153,11 @@ class Fish:
         return self.closest_food
 
     def get_closest_fish(self, fish):
-        self.closest_fish = self.get_closest_ent(fish)
+        if len(fish) < 1:
+            return self
+        looklist = fish.copy()
+        looklist.remove(self)
+        self.closest_fish = self.get_closest_ent(looklist)
         return self.closest_fish
 
     def advance_toward(self, point: tuple, strength: int = 2):
@@ -139,20 +165,18 @@ class Fish:
         x, y = self.pos
 
         if x < point[0]:
-            x += strength
+            if self.is_within((x + strength, y)):
+                self.move(strength, 0)
         elif x > point[0]:
-            x -= strength
-        else:
-            pass
+            if self.is_within((x - strength, y)):
+                self.move(-strength, 0)
 
         if y < point[1]:
-            y += strength
+            if self.is_within((x, y + strength)):
+                self.move(0, strength)
         elif y > point[1]:
-            y -= strength
-        else:
-            pass
-
-        self.pos = (x, y)
+            if self.is_within((x, y - strength)):
+                self.move(0, -strength)
 
     def advance_away(self, point: tuple, strength: int = 2):
         self.advance_toward(point, -strength)
@@ -172,37 +196,46 @@ class Fish:
             self.eat()
             foods.remove(self.closest_food)
 
-    def wander(self, fish, food, bubbles):
-        seed = random.randint(0, 3)
+    def get_fish_image(self):
+        return pygame.transform.flip(pygame.transform.scale(FISH_IMAGES.get(self.personality), (64, 64)),
+                                     self.facing_left, False)
+
+    def wander(self, bubbles, fish):
+
         if self.get_distance_from_self(self.wander_goal) < 4:
             self.assign_new_wander_goal()
-        if seed == 1:
-            if self.personality == 3:
-                if len(bubbles) > 0:
-                    self.wander_goal = self.get_closest_ent(bubbles).pos
-                else:
-                    self.get_random_map_point()
-            self.advance_toward(self.wander_goal, 2)
 
-        elif seed == 2:
-            self.get_closest_fish(fish)
-            if self.personality == 1:  # Timid:
-                if self.get_distance_from_self(self.closest_fish.pos) < 30:
-                    self.advance_away(self.closest_fish.pos, 12)
-            elif self.personality == 2:  # Friendly
+        # All fish actively avoid bubble chasers
+        if self.personality != 3 and self.closest_fish and self.closest_fish.personality == 3 and self.get_distance_to_target(
+                self.closest_fish) < 50:
+            self.advance_away(self.closest_fish.pos, 6)
+
+        if self.personality == 0:
+            self.advance_toward(self.wander_goal)
+        elif self.personality == 1:  # Timid:
+            if self.get_distance_to_target(self.closest_fish) < 80 and self.closest_fish.personality != 1:
+                self.advance_away(self.closest_fish.pos, 3)
+            else:
+                self.advance_toward(self.wander_goal)
+        elif self.personality == 2:  # Friendly
+            if self.get_distance_to_target(self.closest_fish) > 150:
                 self.advance_toward(self.closest_fish.pos)
-            elif self.personality == 3:
-                self.advance_toward(self.wander_goal, 6)
-            self.pos = (self.pos[0] + random.randint(-1, 1), self.pos[1] + random.randint(-1, 1))
-        else:
-            pass
+            else:
+                self.advance_toward(self.wander_goal)
+        elif self.personality == 3:  # Bubble Chaser
+            if len(bubbles) > 0:
+                self.advance_toward(self.get_closest_ent(bubbles).pos, 3)
+            else:
+                self.advance_toward(self.wander_goal)
 
-    def draw(self, screen, color, font):
+    def draw_nametag(self, screen, color, font):
         nametag_rect = pygame.Rect(self.pos[0], self.pos[1] - 15, 20, 10)
-        nametag_label = font.render(f'Fish{self.name} | {self.hunger} | {self.personality}', True, color,
+        nametag_label = font.render(f'Fish{self.name} | {self.hunger} |', True, color,
                                     )
         screen.blit(nametag_label, nametag_rect)
-        pygame.draw.rect(screen, color, (*self.pos, 18, 18))
+
+    def draw(self, screen):
+        screen.blit(self.get_fish_image(), (self.pos[0] - 16, self.pos[1] - 16))
 
     def draw_food_target(self, screen, color, foods):
         self.get_closest_ent(foods)
@@ -216,7 +249,9 @@ class Fish:
 
     def run(self, fish: list, foods: list[Food], bubbles: list[Bubble]):
         self.increase_hunger()
+        self.get_closest_fish(fish)
+        self.get_closest_food(foods)
         if self.is_hungry() and len(foods) > 0:
             self.seek_food(foods)
         else:
-            self.wander(fish, foods, bubbles)
+            self.wander(bubbles, fish)
--- FishBowl.py
+++ FishBowl.py
@@ -6,34 +6,81 @@ D_PERSONALITY = 0
 D_SHOW_GOAL = False
 D_SHOW_PATH = False
 D_BUBBLES = False
+D_NAMETAG = True
+
+FPS_CAP = 60
 
 pygame.init()
+pygame.mixer.init()
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+OVERLAY_SURFACE = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
+
 pygame.display.set_caption("Fish Bowl Sim")
 
 FONT = pygame.font.SysFont("Consolas", 16, True)
 
+timer = pygame.time.Clock()
+
+IMAGES = {
+
+}
+
 COLORS = {
     "background": (115, 175, 255),
+    "distant_background": (80, 140, 220),
     "trail": (255, 255, 255),
     "food": (255, 255, 0),
     "fish": (100, 10, 10)
 }
 
 PERSONALITY_COLORS = [
-    (100, 10, 10),  # Neutral
+    (255, 150, 10),  # Neutral
     (80, 80, 80),  # Timid
     (80, 250, 100),  # Friendly
     (150, 30, 150)  # Bubble Chaser
 ]
 
+personalities = {
+    0: {"name": "Neutral",
+        "description": "This fish has no preference for where to be. They will wander the tank aimlessly "
+                       "and chase food when they are hungry."},
+    1: {"name": "Timid",
+        "description": "This fish prefers to stay away from the others. They will hang around the corners of the "
+                       "tank and chase food only if another fish isn't going for it already."},
+    2: {"name": "Friendly",
+        "description": "This fish prefers to stay near the others. They will hang around the middle of the tank, "
+                       "and race other fish to get food."},
+    3: {"name": "Bubble Chaser",
+        "description": "This fish does not wander like the rest. They only want to pop bubbles. They will eat "
+                       "only when necessary. All fish actively avoid the bubble chasers -- they don't seem stable."}
+}
+
 
 def clear_screen():
+    top_left = (WINDOW_WIDTH // 8, WINDOW_HEIGHT // 8)
+    top_right = (WINDOW_WIDTH - WINDOW_WIDTH // 8, WINDOW_HEIGHT // 8)
+    bottom_left = (WINDOW_WIDTH // 4, WINDOW_HEIGHT - WINDOW_HEIGHT // 4)
+    bottom_right = (WINDOW_WIDTH - WINDOW_WIDTH // 4, WINDOW_HEIGHT - WINDOW_HEIGHT // 4)
     WINDOW.fill(COLORS["background"])
+    WINDOW.blit(OVERLAY_SURFACE, (0, 0))
+    OVERLAY_SURFACE.fill((0, 0, 0, 0))
+
+    pygame.draw.rect(WINDOW, COLORS["distant_background"], (
+        WINDOW_WIDTH // 8, WINDOW_HEIGHT // 8, WINDOW_WIDTH - WINDOW_WIDTH // 4,
+        WINDOW_HEIGHT - WINDOW_HEIGHT // 4))
+    pygame.draw.line(OVERLAY_SURFACE, (255, 255, 255, 80), (0, 0), top_left, 4)
+    pygame.draw.line(OVERLAY_SURFACE, (255, 255, 255, 80), (0, WINDOW_HEIGHT), bottom_left, 4)
+    pygame.draw.line(OVERLAY_SURFACE, (255, 255, 255, 80), (WINDOW_WIDTH, 0), top_right, 4)
+    pygame.draw.line(OVERLAY_SURFACE, (255, 255, 255, 80), (WINDOW_WIDTH, WINDOW_HEIGHT), bottom_right, 4)
+
+
+def draw_shadow(x):
+    for i in range(10):
+        pygame.draw.ellipse(OVERLAY_SURFACE, (0, 0, 0, (i * 3)), (x - (50 - i * 5), WINDOW_HEIGHT - (50 - i), 100 - (i * 10), 30 - (i * 3)), 1)
 
 
 def get_random_map_point() -> tuple[int, int]:
@@ -45,7 +92,7 @@ def is_in_map_bounds(x, y) -> bool:
 
 
 def handle_events():
-    global RUNNING, fishes, D_PERSONALITY, D_BUBBLES, D_SHOW_GOAL, D_SHOW_PATH
+    global RUNNING, fishes, D_PERSONALITY, D_BUBBLES, D_SHOW_GOAL, D_SHOW_PATH, D_NAMETAG
 
     for event in pygame.event.get():
         match event.type:
@@ -55,27 +102,35 @@ def handle_events():
                 if event.button == 1:
                     food.append(Fish.Food(pygame.mouse.get_pos(), WINDOW_HEIGHT))
                 if event.button == 3:
-                    fishes.append(Fish.Fish(len(fishes), pygame.mouse.get_pos(), (WINDOW_WIDTH, WINDOW_HEIGHT),
+                    fishes.append(Fish.Fish(len(fishes), pygame.mouse.get_pos(), (WINDOW_WIDTH, WINDOW_HEIGHT - 100),
                                             personality=D_PERSONALITY))
             case pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     fishes = []
                 if event.key == pygame.K_LEFT:
-                    D_PERSONALITY -= 1
+                    D_PERSONALITY = max(D_PERSONALITY - 1, 0)
                 if event.key == pygame.K_RIGHT:
-                    D_PERSONALITY += 1
+                    D_PERSONALITY = min(D_PERSONALITY + 1, len(personalities) - 1)
                 if event.key == pygame.K_c:
                     D_BUBBLES = not D_BUBBLES
                 if event.key == pygame.K_z:
                     D_SHOW_PATH = not D_SHOW_PATH
                 if event.key == pygame.K_x:
                     D_SHOW_GOAL = not D_SHOW_GOAL
+                if event.key == pygame.K_v:
+                    D_NAMETAG = not D_NAMETAG
 
 
 def create_bubbles(percent_chance: float):
     for i in range(0, WINDOW_WIDTH):
         if random.random() <= percent_chance:
             bubbles.append(Fish.Bubble((i, WINDOW_HEIGHT), random.randint(6, 30)))
+
+
+def draw_tank_overlay():
+    for i in range(255):
+        pygame.draw.circle(OVERLAY_SURFACE, (120, 120, 120, i), (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2),
+                           (WINDOW_WIDTH // 2) + i, 2)
 
 
 RUNNING = True
@@ -87,6 +142,7 @@ bubbles: list[Fish.Bubble] = []
 
 def main():
     while RUNNING:
+        timer.tick(FPS_CAP)
         handle_events()
         clear_screen()
 
@@ -95,15 +151,18 @@ def main():
 
         for fish in fishes:
             fish.run(fishes, food, bubbles)
-            fish.draw(WINDOW, PERSONALITY_COLORS[fish.personality], FONT)
+            fish.draw(WINDOW)
+            if D_NAMETAG:
+                fish.draw_nametag(WINDOW, PERSONALITY_COLORS[fish.personality], FONT)
             if D_SHOW_PATH:
                 fish.draw_line_to_wander_goal(WINDOW, (255, 255, 255))
             if D_SHOW_GOAL:
                 fish.draw_wander_goal(WINDOW, PERSONALITY_COLORS[fish.personality])
+            draw_shadow(fish.pos[0])
 
         for pellet in food:
-            pellet.fall()
-            pellet.draw(WINDOW, COLORS["food"])
+            pellet.fall(food)
+            pellet.draw(WINDOW)
 
         for bubble in bubbles:
             bubble.rise(bubbles)
@@ -117,8 +176,9 @@ def main():
             (255, 255, 255, 80), True)
         WINDOW.blit(settings_label, settings_rect)
 
+        # draw_tank_overlay()
         pygame.display.update()
-        pygame.time.delay(10)
+        pygame.time.delay(8)
 
 
 main()
